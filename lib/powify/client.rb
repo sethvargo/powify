661fa82c505ffec73a258c8000a1d6e782d4755d
Seth Vargo <sethvargo@gmail.com>
2011-08-10 01:34:21 -0400

    fixing whitespace issues

--- lib/powify/client.rb
+++ lib/powify/client.rb
@@ -10,10 +10,10 @@ module Powify
         rescue Exception => e
           $stdout.puts e
         end
-        
+
         help
       end
-      
+
       def help
         $stdout.puts ""
         $stdout.puts "  [SERVER COMMANDS]"
@@ -28,8 +28,8 @@ module Powify
         $stdout.puts "    powify server status            print the current server status"
         $stdout.puts "    powify server config            print the current server configuration"
         $stdout.puts "    powify server logs              tails the pow server logs"
-        $stdout.puts ""                                 
-        $stdout.puts "  [APP COMMANDS]"                 
+        $stdout.puts ""
+        $stdout.puts "  [APP COMMANDS]"
         $stdout.puts "    powify create [NAME]            creates a pow app from the current directory"
         $stdout.puts "    powify destroy [NAME]           destroys the pow app linked to the current directory"
         $stdout.puts "    powify restart [NAME]           restarts the pow app linked to the current directory"
@@ -39,7 +39,7 @@ module Powify
         $stdout.puts "    powify environment [ENV]        run the this pow app in a different environment (aliased `env`)"
         $stdout.puts "    powify browse [NAME]            opens and navigates the default browser to this app"
         $stdout.puts "    powify logs [NAME]              tail the application logs"
-        $stdout.puts ""                               
+        $stdout.puts ""
       end
     end
   end
--- lib/powify/server.rb
+++ lib/powify/server.rb
@@ -4,16 +4,16 @@ require 'json'
 # invoked via pow server [COMMAND] [ARGS]
 module Powify
   class Server
-    
+
     AVAILABLE_METHODS = %w(install reinstall update uninstall remove start stop restart status config list logs help)
-    
+
     class << self
       def run(args = [])
         method = args[0].to_s.downcase
         raise "The command `#{args.first}` does not exist for pow server!" unless Powify::Server::AVAILABLE_METHODS.include?(method)
         self.send(method)
       end
-      
+
       protected
       # Install the POW server
       def install
@@ -23,7 +23,7 @@ module Powify
       end
       alias_method :reinstall, :install
       alias_method :update, :install
-      
+
       # Uninstall the POW server
       def uninstall
         $stdout.puts "Uninstalling/Removing pow server..."
@@ -31,27 +31,27 @@ module Powify
         $stdout.puts "Done!"
       end
       alias_method :remove, :uninstall
-      
+
       # Start the POW server (command taken from 37 Signals installation script)
       def start
         $stdout.puts "Starting the pow server..."
         %x{launchctl load "$HOME/Library/LaunchAgents/cx.pow.powd.plist"}
         $stdout.puts "Done!"
       end
-      
+
       # Stop the POW server (command taken from 37 Signals installation script)
       def stop
         $stdout.puts "Stopping the pow server..."
         %x{launchctl unload "$HOME/Library/LaunchAgents/cx.pow.powd.plist"}
         $stdout.puts "Done!"
       end
-      
+
       # Restart the POW server
       def restart
         stop
         start
       end
-      
+
       # Print the current POW server status
       def status
         $stdout.puts "The current status of the pow server is:\n\n"
@@ -60,7 +60,7 @@ module Powify
         json.each_pair { |k,v| $stdout.puts "  #{k}: #{v}" }
         $stdout.puts "\n"
       end
-      
+
       # Print the current POW server configuration
       def config
         $stdout.puts "The current configuration of the pow server is:\n\n"
@@ -69,14 +69,14 @@ module Powify
         json.each_pair {|k,v| $stdout.puts "  #{k}: #{v}"}
         $stdout.puts "\n"
       end
-      
+
       # List all active POW applications currently on the server
       def list
         $stdout.puts "The following POW applications are available:\n\n"
         Dir["#{POWPATH}/*"].each { |a| $stdout.puts "  #{File.basename(a)} -> #{File.readlink(a)}" }
         $stdout.puts "\nRun `powify open [APP_NAME]` to browse an app"
       end
-      
+
       # tail the server logs
       def logs
         system "tail -f ~/Library/Logs/Pow/access.log"

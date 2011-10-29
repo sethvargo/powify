974f526167eaa91691da8185b2bfcaef014b8494
Warwick Poole <wpoole@gmail.com>
2011-10-29 15:37:54 -0400

    make tests pass for AVAILABLE_METHODS

--- test/test_powify.rb
+++ test/test_powify.rb
@@ -7,7 +7,7 @@ class TestPowify < Test::Unit::TestCase
     end
     
     should 'have all available methods' do
-      assert_equal Powify::App::AVAILABLE_METHODS, %w(create link new destroy unlink remove restart browse open rename logs help)
+      assert_equal Powify::App::AVAILABLE_METHODS, %w(create link new destroy unlink remove restart always_restart always_restart_off browse open rename environment env logs help)
     end
   end
 end
